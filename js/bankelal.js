cd48e466d1d75cab64e4615423dc8daf680f74bd
Harendra <warboss9230@gmail.com>
2026-02-14 04:44:55 +0530

    Create bankelal.js

---
+++ js/bankelal.js
@@ -0,0 +1,37 @@
+// ===== Header Menu =====
+const menuBtn = document.getElementById("menuBtn");
+const dropdown = document.getElementById("dropdownMenu");
+
+menuBtn.addEventListener("click", () => {
+  dropdown.style.display =
+    dropdown.style.display === "flex" ? "none" : "flex";
+});
+
+// ===== Search Overlay =====
+const searchIcon = document.querySelector(".search-icon");
+const searchOverlay = document.getElementById("searchOverlay");
+
+searchIcon.addEventListener("click", () => {
+  searchOverlay.style.display = "flex";
+  setTimeout(() => {
+    searchOverlay.classList.add("active");
+  }, 50);
+});
+
+searchOverlay.addEventListener("click", (e) => {
+  if (e.target === searchOverlay) {
+    searchOverlay.style.display = "none";
+    searchOverlay.classList.remove("active");
+  }
+});
+
+// Hover glow effect
+document.querySelectorAll(".comic-card").forEach(card => {
+    card.addEventListener("mouseenter", () => {
+        card.style.boxShadow = "0 0 30px #ff8800";
+    });
+
+    card.addEventListener("mouseleave", () => {
+        card.style.boxShadow = "0 0 15px #ff8800";
+    });
+});
