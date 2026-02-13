b14504cb01be9d7b74c316785f95a51fcf30d4c5
Harendra <warboss9230@gmail.com>
2026-02-14 02:46:40 +0530

    Create dhruva.js

---
+++ js/dhruva.js
@@ -0,0 +1,11 @@
+// Dhruva Page JS
+
+console.log("Dhruva page loaded");
+
+// Example: Comic click alert (optional)
+
+document.querySelectorAll(".comic-card").forEach(card => {
+    card.addEventListener("click", () => {
+        alert("Comic detail page coming soon!");
+    });
+});
