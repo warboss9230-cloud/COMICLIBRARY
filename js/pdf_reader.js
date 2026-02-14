d59abd9d4f349e737f4856de03e18620113ebc92
Harendra <warboss9230@gmail.com>
2026-02-14 10:47:19 +0530

    Update pdf_reader.js

--- js/pdf_reader.js
+++ js/pdf_reader.js
@@ -1,21 +1,19 @@
-// Get URL parameters
 const params = new URLSearchParams(window.location.search);
+
 const hero = params.get("hero");
 const comic = params.get("comic");
+const driveId = params.get("drive");
 
-if (!hero || !comic) {
-    alert("Comic not found");
+const viewer = document.getElementById("pdfViewer");
+
+// Google Drive PDF
+if (driveId) {
+    viewer.src = `https://drive.google.com/file/d/${driveId}/preview`;
 }
-
-// Set title
-document.getElementById("comicTitle").innerText =
-    comic.replace(".pdf", "").replaceAll("_", " ");
-
-// Set PDF path
-document.getElementById("pdfViewer").src =
-    `../pdf/${hero}/${comic}`;
-
-// Back button
-function goBack() {
-    history.back();
+// Local PDF
+else if (hero && comic) {
+    viewer.src = `../pdf/${hero}/${comic}`;
+}
+else {
+    alert("PDF not found");
 }
